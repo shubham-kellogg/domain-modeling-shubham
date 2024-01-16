57f6576845c70f9e8440d38b79f0e0ef4fc0e1ef
Shubham Bajaj <shubham.bajaj@kellogg.northwestern.edu>
2024-01-16 17:13:28 +0000

    crm_data modeling

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,40 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS salespeople;
+
 -- CREATE TABLES
+
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  email TEXT,
+  address TEXT,
+  phone_number TEXT,
+  company_id INTEGER,
+  activity_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  method TEXT,
+  date_time TEXT,
+  notes TEXT,
+  contact_id INTEGER,
+  salesperson_id INTEGER
+);
+
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  email TEXT
+);
